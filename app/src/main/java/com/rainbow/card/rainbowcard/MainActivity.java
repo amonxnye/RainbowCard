809f9d98738d3c0643809a7df867b94177cf9c41
Amon Nyesigye <amonxnye@gmail.com>
2016-09-13 02:49:54 -0700

    commit
    commit from desktop

---
+++ app/src/main/java/com/rainbow/card/rainbowcard/MainActivity.java
@@ -0,0 +1,248 @@
+package com.rainbow.card.rainbowcard;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private static final String TAG = "MainActivity";
+
+    EditText emailx, passwordx;
+   public String emailz, passwordz,email;
+    Button signin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+       // onTokenRefresh();
+
+        signin = (Button) findViewById(R.id.button);
+        emailx = (EditText) findViewById(R.id.email);
+        passwordx = (EditText) findViewById(R.id.password);
+
+        emailz = emailx.getText().toString();
+        passwordz = passwordx.getText().toString();
+        //firbase authentication
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                     email = user.getEmail();
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                   // Toast.makeText(MainActivity.this, " Logged in", Toast.LENGTH_SHORT).show();
+
+                    shiftPage();
+
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    Toast.makeText(MainActivity.this, " Login failure", Toast.LENGTH_SHORT).show();
+                    //signInRegisteredUser("amonxnyee@gmail.com", "anitadavid");
+                }
+                // ...
+            }
+        };
+
+        // ...
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              /*  Snackbar.make(v, emailx.getText().toString(), Snackbar.LENGTH_SHORT)
+                        .setAction("Action", null).show();*/
+                try {
+      Snackbar.make(v, "Trying ...", Snackbar.LENGTH_SHORT)
+                        .setAction("Action", null).show();
+
+                    mAuth.signInWithEmailAndPassword( emailx.getText().toString(),passwordx.getText().toString())
+                            .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                                    // If sign in fails, display a message to the user. If sign in succeeds
+                                    // the auth state listener will be notified and logic to handle the
+                                    // signed in user can be handled in the listener.
+                                    if (!task.isSuccessful()) {
+                                        Log.w(TAG, "signInWithEmail:failed", task.getException());
+                                        Toast.makeText(MainActivity.this, "Failed Login Attempt", Toast.LENGTH_SHORT).show();
+                                       /* Intent intent = new Intent(MainActivity.this,PayActivity.class);
+                                        startActivity(intent);*/
+
+                                    }
+
+                                    // ...
+                                }
+                            });
+                }catch (Exception e){
+
+                    Toast.makeText(MainActivity.this,e.toString(), Toast.LENGTH_LONG).show();
+
+                }
+            }
+
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        FloatingActionButton fab2 = (FloatingActionButton) findViewById(R.id.fab2);
+        fab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+
+    public void signInRegisteredUser(String emaily, String passwordy) {
+
+        mAuth.signInWithEmailAndPassword(emaily, passwordy)
+                .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail:failed", task.getException());
+                            Toast.makeText(MainActivity.this, "Failed Login Attempt", Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+    public void shiftPage(){
+        Intent intent = new Intent(this, PayActivity.class);
+        intent.putExtra("email", email);
+       // intent.putExtra("lastName", "Your Last Name Here");
+        startActivity(intent);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+            Toast.makeText(MainActivity.this, "This is the Drawer", Toast.LENGTH_SHORT).show();
+
+        } else if (id == R.id.nav_slideshow) {
+
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ app/src/main/java/com/rainbow/card/rainbowcard/PayActivity.java
@@ -0,0 +1,391 @@
+package com.rainbow.card.rainbowcard;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.Console;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Iterator;
+
+import javax.net.ssl.HttpsURLConnection;
+
+/**
+ * Created by Amon on 9/11/2016.
+ */
+public class PayActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private static final String TAG = "PayActivity";
+    public Button pay;
+    public  EditText amount,card;
+    public InputStream is;
+    public TextView balance;
+    public String amountx,cardx,emailforbalance;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_tow);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+
+        pay = (Button)findViewById(R.id.pay);
+        balance = (TextView) findViewById(R.id.balance);
+       // card = (EditText)findViewById(R.id.card);
+      //  message = (Button)findViewById(R.id.fab) ;
+       // about = (Button)findViewById(R.id.fab2);
+      //  amountx = amount.getText().toString();
+       // cardx = card.getText().toString();
+        //firbase authentication
+        mAuth = FirebaseAuth.getInstance();
+
+
+
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                   Toast.makeText(PayActivity.this, " Logged in", Toast.LENGTH_LONG).show();
+                    emailforbalance = user.getEmail();
+                    new ResponseBalanceData().execute(emailforbalance);
+
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    Toast.makeText(PayActivity.this, " Login failure", Toast.LENGTH_SHORT).show();
+
+                    shiftPage();
+                }
+                // ...
+            }
+        };
+
+       // new ResponseBalanceData().execute(emailforbalance);
+
+pay.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+       // dataprocess();
+        pay = (Button)findViewById(R.id.pay);
+        amount = (EditText)findViewById(R.id.amount);
+        card = (EditText)findViewById(R.id.card);
+        //  message = (Button)findViewById(R.id.fab) ;
+        // about = (Button)findViewById(R.id.fab2);
+        amountx = amount.getText().toString();
+        cardx = card.getText().toString();
+
+        new ResponsePaymentData().execute(amountx,cardx);
+        new ResponseBalanceData().execute(emailforbalance);
+    }
+});
+
+
+    }
+
+
+
+    public class ResponsePaymentData extends AsyncTask<String, Void, String> {
+
+
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            // These two need to be declared outside the try/catch
+
+            String response = null;
+            Log.d(TAG, params[0]);
+            Log.d(TAG, params[1]);
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are avaiable at OWM's forecast API page, at
+                URL url = new URL("http://ec2-54-191-230-33.us-west-2.compute.amazonaws.com/rainbow/view/codepay.php");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(10000);
+                conn.setConnectTimeout(15000);
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                Intent intent = getIntent();
+
+                String email = intent.getStringExtra("email");
+
+                Uri.Builder builder = new Uri.Builder()
+                        .appendQueryParameter("amount",params[0])
+                        .appendQueryParameter("card",params[1])
+                        .appendQueryParameter("email",email);
+                        //.appendQueryParameter("card_buyer", "2016");
+                String query = builder.build().getEncodedQuery();
+
+                OutputStream os = conn.getOutputStream();
+                // InputStream is = conn.getInputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(query);
+                writer.flush();
+                writer.close();
+                os.close();
+
+                conn.connect();
+
+               // Toast.makeText(PayActivity.this,"Payment Done...", Toast.LENGTH_SHORT).show();
+
+
+                // Read the input stream into a String
+                InputStream inputStream = conn.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                BufferedReader  reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                response = buffer.toString();
+                Log.d(TAG, response);;
+               // return response;
+
+               // Toast.makeText(PayActivity.this,response, Toast.LENGTH_SHORT).show();
+
+            } catch (Exception e) {
+              //  Log.e( e.toString());
+                Toast.makeText(PayActivity.this,"Failed Payment", Toast.LENGTH_SHORT).show();
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Toast.makeText(PayActivity.this,s, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+    public class ResponseBalanceData extends AsyncTask<String, Void, String> {
+
+
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            // These two need to be declared outside the try/catch
+
+            String response = null;
+            //Log.d(TAG, params[0]);
+            //Log.d(TAG, params[1]);
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are avaiable at OWM's forecast API page, at
+                URL url = new URL("http://ec2-54-191-230-33.us-west-2.compute.amazonaws.com/rainbow/view/balancetry.php");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(10000);
+                conn.setConnectTimeout(15000);
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+             //   Intent intent = getIntent();
+
+               // String email = intent.getStringExtra("email");
+
+                Uri.Builder builder = new Uri.Builder()
+                        .appendQueryParameter("email",params[0]);
+                //.appendQueryParameter("card_buyer", "2016");
+                String query = builder.build().getEncodedQuery();
+
+                OutputStream os = conn.getOutputStream();
+                // InputStream is = conn.getInputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(query);
+                writer.flush();
+                writer.close();
+                os.close();
+
+                conn.connect();
+
+                // Toast.makeText(PayActivity.this,"Payment Done...", Toast.LENGTH_SHORT).show();
+
+
+                // Read the input stream into a String
+                InputStream inputStream = conn.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                BufferedReader  reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                response = buffer.toString();
+                Log.d(TAG, response);
+                // return response;
+
+                // Toast.makeText(PayActivity.this,response, Toast.LENGTH_SHORT).show();
+
+            } catch (Exception e) {
+                //  Log.e( e.toString());
+                Toast.makeText(PayActivity.this,"No data Currently", Toast.LENGTH_SHORT).show();
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return "0";
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+          balance.setText(s);
+            //Toast.makeText(PayActivity.this,s, Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    public void logout(View v) {
+
+        FirebaseAuth.getInstance().signOut();
+       // Toast.makeText(PayActivity.this, " Logout", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void RefreshData(View v) {
+
+       // FirebaseAuth.getInstance().signOut();
+       // Toast.makeText(PayActivity.this, " Logout", Toast.LENGTH_SHORT).show();
+        new ResponseBalanceData().execute(emailforbalance);
+
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    public void shiftPage(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+            Toast.makeText(PayActivity.this, "This is the Drawer", Toast.LENGTH_SHORT).show();
+
+        } else if (id == R.id.nav_slideshow) {
+
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+
+}
